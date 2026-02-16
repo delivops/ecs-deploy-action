f7882c747b91ae3934c213089e21a54c61fb7c69
Shlomi <shlomo@delivops.com>
2026-02-16 19:29:08 +0200

    split to smaller files

--- scripts/generate_task_def.py
+++ scripts/generate_task_def.py
@@ -1,29 +1,5 @@
 #!/usr/bin/env python3
-"""Compatibility wrapper for task definition generation.
 
-This module re-exports the public API while delegating implementation to
-`scripts/task_def/` modules.
-"""
-
-from task_def import (  # noqa: F401
-    ContainerBuilder,
-    LogLevel,
-    SecretManager,
-    TaskConfig,
-    ValidationError,
-    build_app_container,
-    build_fluent_bit_container,
-    build_image_uri,
-    build_init_containers,
-    build_linux_parameters,
-    build_otel_container,
-    generate_task_definition,
-    load_and_validate_config,
-    parse_args,
-    parse_image_parts,
-    setup_logging,
-    validate_config,
-)
 from task_def.cli import main
 
 
